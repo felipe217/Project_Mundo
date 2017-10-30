f770ce6eaef57983ead9197d41f46038a03e856a
felipe217 <felipealvarez487@gmail.com>
2017-10-29 21:48:22 -0600

    Add files via upload

---
+++ Web_site/sql/Insertar_desde_admin.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'Conexion.php';
+
+class Insertar_usuario{
+    public $identificacion;
+    public $nombres;
+    public $apellidos;
+    public $nacimiento;
+    public $domicilio;
+    public $pais;
+    public $cargo;
+    public $usuario;
+    public $contrasena;
+    public $codTipoUsuario = 1;
+
+    public function Insertar(){
+
+        $model = new Conexion();
+        $conexion = $model->conectar();
+
+        $sql = "INSERT INTO tblUsuarios (identificacion, nombres, apellidos, nacimiento, domicilio, pais, cargo, usuario, contrasena, codTipoUsuario) VALUES ";
+        $sql .= "(:identificacion, :nombres, :apellidos, :nacimiento, :domicilio, :pais, :cargo, :usuario, :contrasena, :codTipoUsuario)";
+
+        $consulta = $conexion->prepare($sql);
+        $consulta->bindParam(':identificacion', $this->identificacion);
+        $consulta->bindParam(':nombres', $this->nombres);
+        $consulta->bindParam(':apellidos', $this->apellidos);
+        $consulta->bindParam(':nacimiento', $this->nacimiento);
+        $consulta->bindParam(':domicilio', $this->domicilio);
+        $consulta->bindParam(':pais', $this->pais);
+        $consulta->bindParam(':cargo', $this->cargo);
+        $consulta->bindParam(':usuario', $this->usuario);
+        $consulta->bindParam(':contrasena', $this->contrasena);
+        $consulta->bindParam(':codTipoUsuario', $this->codTipoUsuario);
+        $consulta->execute();
+
+        // Redireccionando despues de la insercion
+        header('location: panelusuarios.php');
+    }
+}
+
+?>
---
+++ Web_site/sql/loginuser.php
@@ -0,0 +1,44 @@
+<?php
+require 'Conexion.php';
+
+class loginuser{
+
+    public $username;
+    public $password;
+    public $mensaje;
+
+    public function login(){
+
+        $model = new Conexion;
+        $conexion = $model->conectar();
+        $sql = 'SELECT * FROM tblUsuarios WHERE usuario=:username AND contrasena=:password';
+        $consulta = $conexion->prepare($sql);
+        $consulta->bindParam(':username', $this->username, PDO::PARAM_STR);
+        $consulta->bindParam(':password', $this->password, PDO::PARAM_STR);
+        $consulta->execute();
+        $total = $consulta->rowCount();
+
+        if($total == 0)
+            {
+                $this->mensaje = 'Error al iniciar sesion, datos incorrectos :(';
+            }
+        else
+            {
+                $fila = $consulta->fetch();
+                session_start();
+                $_SESSION['login'] = true;
+                $_SESSION['tipo'] = $fila['codTipoUsuario'];
+                $_SESSION['username'] = $fila['usuario'];
+                $_SESSION['name'] = $fila['nombres'];
+                $_SESSION['lastname'] = $fila['apellidos'];
+
+                if($_SESSION['tipo'] == 1){
+                  header('location: panelusuarios.php');
+            		}
+                else{
+                  header('location: perfil.html');
+                }
+            }
+    }
+}
+?>
