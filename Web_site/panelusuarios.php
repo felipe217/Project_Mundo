3a1ca827c7f1bc8f3e30659c44abe0cc3d055b77
felipe217 <felipealvarez487@gmail.com>
2017-11-07 20:17:49 -0600

    Update panelusuarios.php

--- Web_site/panelusuarios.php
+++ Web_site/panelusuarios.php
@@ -210,8 +210,8 @@ if(isset($_POST['save_user']))
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		<ul class="nav navbar-nav">
-			<li>
-				<a href="panelusuarios.html">
+			<li class="menu-activo">
+				<a href="panelusuarios.php">
 				<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
 					Usuarios 
 				</a>
@@ -222,7 +222,7 @@ if(isset($_POST['save_user']))
 					Proyectos  
 				</a>
 			</li>
-			<li class="menu-activo">
+			<li>
 				<a href="panelrecursos.html">
 				<span class="glyphicon glyphicon-eur" aria-hidden="true"></span>
 					Recursos 
